68fb90ba4fce6524004593e487f8bbdf2c93a418
generic_human <35752279+aGenericHuman@users.noreply.github.com>
2018-06-16 00:06:48 -0700

    refactored for cleaner code and changed prompts.

--- matrixing.py
+++ matrixing.py
@@ -1,12 +1,9 @@
 # function based full dice roller with statistics
 import random
+#ask Bette if there is anything else I am messing up on
+print("welcome to the dice roller 2018, an example roll is 2d4 + 3d6 - 4\n")
 
-
-def roll_it(die):
-    return random.randint(1, die)
-
-
-minimum_val = 0
+minimum_val = 0 # these are for statistics
 max_val = 0
 
 
@@ -17,33 +14,39 @@ def xdx_eval(given_str, sign):
         split_index = given_str.index("d")
         num_of_dies = int(given_str[:split_index])
         die_val = int(given_str[split_index + 1:])
-        for tro in range(num_of_dies):
-            if sign:
-                minimum_val += 1
-                max_val += die_val
-            else:
-                minimum_val -= die_val
-                max_val -= 1
-            current_roll = roll_it(die_val)
-            if current_roll != die_val:
-                print("You rolled a {0} and got a {1}".format(die_val, current_roll))
-            else:
+        if num_of_dies != 1:
+            print("\nrolling {} {} sided dice...".format(num_of_dies, die_val))
+        else:
+            print("\nrolling {} {} sided die...".format(num_of_dies, die_val))
+        for _ in range(num_of_dies):  # number of times to roll the dice
+            if get_avg:
+                if sign:
+                    minimum_val += 1
+                    max_val += die_val
+                else:
+                    minimum_val -= die_val
+                    max_val -= 1
+            current_roll = random.randint(1, die_val)  # most important line
+            if current_roll == die_val:
                 print("You got the maximum roll of {0}!".format(current_roll))
+            else:
+                print("You rolled a {1} out of {0}".format(die_val, current_roll))
             total_val += current_roll
         return total_val
     except ValueError:
         print("That input was not valid, try again using notation like 2d8-3d5+5")
-        # inp_chooser()
+
 
 def sign_pls(first_dig):
     if first_dig.isdecimal():
         return True
     elif "+" in first_dig:
         return True
-    elif "-" in first_dig:
+    elif "-" in first_dig:  # I can't bring myself to turn this into an else
         return False
 
-def operator_index(strong):
+
+def operator_index(strong):  # gets the index of the next required split
     pos_incl = "+" in strong
     neg_incl = "-" in strong
     if pos_incl:
@@ -66,15 +69,56 @@ def operator_index(strong):
 repeat_saver = ""
 get_avg = False
 
-def inp_chooser():
-    global get_avg
-    global repeat_saver
-    global minimum_val, max_val
-    while True:
-        input_die = input("enter the number and type of dice you would like to roll? type 'quit' to quit, hit"
-                           " enter to redo the last roll or avg to see the probability of your last roll \n")
+
+def string_sorter(die_choose):
+    global minimum_val, max_val, get_avg
+    grand_tot = 0
+    grp_amt = die_choose.count("+")
+    neg_amt = die_choose.count("-")
+    for i in range(grp_amt + neg_amt + 1): # +1 for if it is just a number
+        if die_choose. replace(" ", "") == "":
+            break
+        front_sign = sign_pls(die_choose[0])
+        if not die_choose[0].isnumeric():
+            die_choose = die_choose[1:]  # gets rid of +- at start of  string
+        next_indx = operator_index(die_choose)
+        is_negative = -1
+        if front_sign:
+            is_negative = 1
+            print("+" + die_choose[:next_indx])
+        if die_choose[:next_indx].isdecimal():  # if the next thing is a number
+            if front_sign:
+                print("+"+die_choose[:next_indx])
+            else:
+                print("-" + die_choose[:next_indx])
+            grand_tot += int(die_choose[:next_indx]) * is_negative
+            minimum_val += int(die_choose[:next_indx]) * is_negative
+            max_val += int(die_choose[:next_indx]) * is_negative
+        else:
+            try:
+                grand_tot += xdx_eval(die_choose[:next_indx], front_sign)*is_negative
+            except TypeError:
+                print()
+                return
+        die_choose = die_choose[next_indx:]
+
+    if get_avg:
+        print("\nthe maximum possible roll for {} is:".format(repeat_saver), max_val)
+        print("the minimum roll is:", minimum_val)
+        print("the average roll is:", (max_val + minimum_val)/2)
+    print("\nyour total roll is:", grand_tot, "\n")
+    # return grand_tot # is for being part of bigger system of files
+
+
+def main():
+    global minimum_val, max_val, get_avg, repeat_saver
+    while True:  # is there a better thing to put this as?
+        input_die = input("enter the number and type of dice you would like "
+            "to roll, type 'quit' to quit, hit enter to redo the last roll "
+            "or 'avg' to see the probability of your last roll \n")
         if "quit" in input_die:
             return
+        input_die.replace(" ", "")
         if input_die == "":
             input_die = repeat_saver
             if input_die == "":
@@ -86,7 +130,7 @@ def inp_chooser():
             if repeat_saver != "":
                 get_avg = True
                 minimum_val = max_val = 0
-                main_func(repeat_saver)
+                string_sorter(repeat_saver)
                 continue
             else:
                 print("you need to put in a calculation before finding its average\n")
@@ -94,53 +138,7 @@ def inp_chooser():
         else:
             get_avg = False
         repeat_saver = input_die
-
-        '''if "d" not in input_die and "avg" not in input_die:
-            print("That input was not valid, try again using notation like 2d8-3d5+5")
-            continue'''
-        main_func(input_die)
+        string_sorter(input_die)
 
 
-def main_func(die_choose):
-    global minimum_val, max_val, get_avg
-    grand_tot = 0
-    grp_amt = die_choose.count("+")
-    neg_amt = die_choose.count("-")
-    for i in range(grp_amt + 1 + neg_amt):
-        front_sign = sign_pls(die_choose[0])
-        if not die_choose[0].isnumeric():
-            die_choose = die_choose[1:]
-        next_indx = operator_index(die_choose)
-        if die_choose[:next_indx].isdecimal():
-            if front_sign:
-                print("+"+die_choose[:next_indx])
-                grand_tot += int(die_choose[:next_indx])
-                minimum_val += int(die_choose[:next_indx])
-                max_val += int(die_choose[:next_indx])
-            else:
-                print("-" + die_choose[:next_indx])
-                grand_tot -= int(die_choose[:next_indx])
-                minimum_val -= int(die_choose[:next_indx])
-                max_val -= int(die_choose[:next_indx])
-            print("INPUT IS CURRENTLY", die_choose)
-        else:
-            try:
-                if front_sign:
-                    grand_tot += xdx_eval(die_choose[:next_indx], True)
-                else:
-                    grand_tot -= xdx_eval(die_choose[:next_indx], False)
-            except TypeError:
-                print()
-                return
-        die_choose = die_choose[next_indx:]
-        if die_choose. replace(" ", "") == "":
-            break
-        # print(die_choose) this is to check if it is truncating properly
-    if get_avg:
-        print("\nthe maximum possible roll for {} is:".format(repeat_saver), max_val)
-        print("the minimum roll is:", minimum_val)
-        print("the average roll is:", (max_val + minimum_val)/2)
-    print("\nyour total roll is:", grand_tot, "\n")
-    #return grand_tot
-
-inp_chooser()
+main()
